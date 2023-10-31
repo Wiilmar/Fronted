23e761223715b211ce89b942e14d8c9615ddccb5
Daniel <danielfelipeluborealpe@gmail.com>
2023-10-31 00:52:31 -0500

    eltal

--- FrontEnd/src/Main.jsx
+++ FrontEnd/src/Main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Motopajero } from "./Primero";
 
+import "./Styles/Styles.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
